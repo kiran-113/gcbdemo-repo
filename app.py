2a61ba4b537c24801f78ac6762c58c66ce9eea85
Kirankumar <82759393+kiran-113@users.noreply.github.com>
2021-10-13 15:29:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello, World! This is kiran Hello</h3>"
+    html = "<h3>Hello, World! This is kiran</h3>"
+    html = '<h1>Hello This is Continues Build Demo</h1>"
     return html
 
 if __name__ == "__main__":
